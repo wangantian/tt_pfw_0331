2594d0ef66d636a8d1b9b33409cb64bcd4d1f346
wangantian <atwangsh@gmail.com>
2026-03-31 23:18:01 -0400

    initial_change

---
+++ src/tt_um_aw_top.v
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_aw_top (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out  = uo_out_reg;  
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+  // List all unused inputs to prevent warnings
+  //wire _unused = &{ena, clk, rst_n, 1'b0};
+  reg [7:0] uo_out_reg;
+  always@(posedge clk) begin 
+	if (!rst_n) begin
+		uo_out_reg<=0;
+	end begin
+		if (ena) begin
+			uo_out_reg <= ui_in+uio_in;  
+		end 
+	end 
+  end 
+endmodule
